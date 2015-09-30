87a3aea6f04e3a4d3babad4b743b3bb52257d2f9
Mark Scripter <markscript@gmail.com>
2015-09-30 17:23:03 -0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import riot from 'riot';
+import recipe from './recipe'
+import recipeEntity from './recipe/recipe.tag'
+import recipes from './recipes'
+import recipesEntity from './recipes/recipes.tag'
+
+riot.tag('recipes',)
